1815574bcd82df9b8feba872f0fef8fbc1f280ac
Meng-MiamiOH <43998638+Meng-MiamiOH@users.noreply.github.com>
2023-05-23 16:52:11 -0400

    nav

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,12 @@
+import { useRouter } from "next/router";
+
 const FilteredEvetsPage = () => {
+  const router = useRouter();
+  const filterData = router.query.slug;
+
+  if (!filterData){
+    return
+  }
   return (
     <div>
       <h1>Filtered Events Page</h1>
